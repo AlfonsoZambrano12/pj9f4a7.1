c0e2735a69c891c08c0df4012c71e7e32f23e556
AlfonsoZambrano12 <463747.clot@fje.edu>
2025-10-26 21:55:57 +0100

    Versió 1.2 del projecte pizzeria

--- codis/pizzeria/app/pizza.php
+++ codis/pizzeria/app/pizza.php
@@ -1,19 +1,20 @@
 <?php
-	$pvp = 8;
-	if (empty($_GET['ingr'])) {
-		echo "Has demanat només la pizza bàsica. El preu és $pvp € (IVA inclòs)<br>";
-		exit(0);
-	}
-	else {
-		$IVA=21;
-		$pvp=((100+$IVA)/100)*(8+count($_GET['ingr']));
-		echo "Has demanat la pizza bàsica amb:<br> ";
-		foreach ($_GET['ingr'] as $ingredient) {
-			if ($ingredient) {
-				echo "* $ingredient<br>";
-			}
-		}
-		echo "El preu és $pvp € (IVA inclòs)<br>";
-		exit(0);
-	}
+        $pvp = 8;
+        $IVA = 21;
+
+        if (empty($_GET['ingr'])) {
+                $pvp_public = $pvp * (1 + $IVA / 100);
+                echo "Has demanat només la pizza bàsica. El preu és $pvp_public € (IVA inclòs)<br>";
+        } else {
+                $pvp_public = (8 + count($_GET['ingr'])) * (1 + $IVA / 100);
+                echo "Has demanat la pizza bàsica amb:<br> ";
+                foreach ($_GET['ingr'] as $ingredient) {
+                        if ($ingredient) {
+                                echo "* $ingredient<br>";
+                        }
+                }
+                echo "El preu és $pvp_public € (IVA inclòs)<br>";
+        }
 ?>
+
+
